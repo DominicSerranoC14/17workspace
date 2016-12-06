9ba79458257fa3c25b5b8b38dffa9753837348da
Dominic Serrano <dmsrojo@gmail.com>
2016-12-06 11:55:00 -0600

    xhrAPI gist completed

--- spa/xhr/xhr101.js
+++ spa/xhr/xhr101.js
@@ -1,14 +1,16 @@
-var outputDiv = document.getElementById('song-div');
+// Reference the output dom element
+var outputDiv = document.getElementById('output-div');
 
-function loadMessages (e) {
+function loadMessages (xhrEvent) {
   // Parse JSON string
-  var data = JSON.parse(e.target.responseText).messages;
+  var data = JSON.parse(xhrEvent.target.responseText).messages;
   var html = "";
 
   // Loop through data, concat messages and author
   for (var i = 0; i < data.length; i++) {
     html += `<p>${data[i].author}: `;
-    html += `${data[i].text}</p>`;
+    html += `${data[i].text}   `;
+    html += `<br>${data[i].date}</p>`;
   }
   // then output html
   outputDiv.innerHTML += html;
---
+++ spa/xhr/xhrAPI.js
@@ -0,0 +1,46 @@
+// Reference the output dom element
+var outputDiv = document.getElementById('output-div');
+// Reference the buttons
+var saveButton = document.getElementById('save-user');
+var displayButton = document.getElementById('display-user');
+// Declare a variable for user info
+var user = "";
+
+
+// This function creates the XHR request and sends it
+function requestUser () {
+  // Create new request object, open and send it
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://randomuser.me/api');
+  request.send();
+  // Add event listener to the XHR load
+  request.addEventListener('load', saveUser);
+};
+
+
+// This function is fired when the XHR request resonds
+function saveUser (e) {
+  // Parse JSON string and store in variable
+  user = JSON.parse(e.target.responseText).results[0];
+  outputDiv.innerHTML = '<br>Your user is loaded! Click display random user button!'
+};
+
+
+function displayUser () {
+  // Built HTML string to output to DOM
+  var html = "";
+  html += `<h1>`;
+  html += `${user.name.title}. ${user.name.first} ${user.name.last} `;
+  html += `<br><img src="${user.picture.large}">`;
+  html += `</h1>`;
+  html += `<p>Click save random user button to display another user!</p>`;
+
+  // Output html to DOM
+  outputDiv.innerHTML = html;
+};
+
+
+// Add event listener to save user button
+saveButton.addEventListener('click', requestUser)
+// Add event listener to display user button
+displayButton.addEventListener('click', displayUser)
