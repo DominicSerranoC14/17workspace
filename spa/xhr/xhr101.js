7e4c1444b2ebfd26d67732b46e80fa6c7ac9a0b1
Dominic Serrano <dmsrojo@gmail.com>
2016-12-06 10:46:45 -0600

    Added comments to XHR dom output file

--- spa/xhr/xhr101.js
+++ spa/xhr/xhr101.js
@@ -1,18 +1,23 @@
 var outputDiv = document.getElementById('song-div');
 
 function loadMessages (e) {
+  // Parse JSON string
   var data = JSON.parse(e.target.responseText).messages;
   var html = "";
 
+  // Loop through data, concat messages and author
   for (var i = 0; i < data.length; i++) {
     html += `<p>${data[i].author}: `;
     html += `${data[i].text}</p>`;
-    outputDiv.innerHTML += html;
   }
+  // then output html
+  outputDiv.innerHTML += html;
 };
 
+// Create new request object, open and send it
 var request = new XMLHttpRequest();
 request.open('GET', 'messages.json');
 request.send();
 
+// Add event listener to the XHR load
 request.addEventListener('load', loadMessages);
