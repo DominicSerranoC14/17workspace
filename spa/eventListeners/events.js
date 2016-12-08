8a586e143fe4a4653379ae4120a4071f7deacc75
Dominic Serrano <dmsrojo@gmail.com>
2016-12-08 14:14:07 -0600

    Finished commenting all dynamic javascript

--- spa/eventListeners/events.js
+++ spa/eventListeners/events.js
@@ -5,36 +5,56 @@ var displayDiv = document.getElementById('display-div');
 // Reference to all button elements on pageload
 var buttonList = document.getElementsByTagName('button');
 
-function activateSingleButton (e) {
+
+
+// When a div button is clicked, the button element and parentElement name are displayed
+function displayButtonClicked (e) {
   displayDiv.innerText = `You clicked ${e.target.innerText}\n`;
   displayDiv.innerText += `${e.target.parentElement.innerText.split(':')[0]} is the parentElement`;
 };
 
-// Add event listeners to all buttons on the page
+
+// Add event listeners to all buttons on the page when page is loaded
 function activateButtons () {
   for (var i = 0; i < buttonList.length; i++) {
-    buttonList[i].addEventListener('click', function(e) {
-      activateSingleButton(e);
-    });
+    // Adding an event listener to each button element
+    buttonList[i].addEventListener('click', displayButtonClicked)
   };
 };
 activateButtons();
 
+
+// Add an event listener on the body element
 body.addEventListener('click', function (e) {
   console.log("//// body eventListener")
+  // logs the target of the element clicked
   console.log("event.target", e.target);
+  // logs the currentTarget of the element clicked
   console.log("event.currentTarget", e.currentTarget);
+  // logs the parentElement of the element clicked
   console.log("Parent", e.target.parentElement);
 });
 
+
+// Event listener for new div button
 newDivButton.addEventListener('click', function(e) {
+  // Use buttonList (which holds all buttons in the DOM) to dynamically increase
+  // the DIV id for a new div
   var newDivId = buttonList.length + 1;
+  // Create a new HTML button element
   var button = document.createElement('button');
-  button.addEventListener('click', activateSingleButton);
+  // Add the displayButtonClicked event listener to the button
+  button.addEventListener('click', displayButtonClicked);
+  // Set the button's text
   button.innerText = `Button ${newDivId}`;
+  // Create a new HTML div element
   var div = document.createElement('div');
+  // Set the div's text
   div.innerText = `DIV ${newDivId}:  `;
-  div.setAttribute('class', 'div');
+  // Add a 'div' class to the new div
+  div.setAttribute('class', 'divs');
+  // Append the button to the div
   div.appendChild(button);
+  // Append the div to the outputDiv
   outputDiv.appendChild(div);
 });
--- spa/eventListeners/loadPage.js
+++ spa/eventListeners/loadPage.js
@@ -1,18 +1,29 @@
+// Reference to the output-div element
 var outputDiv = document.getElementById('output-div');
 
+
+// Function that will populate the DOM dynamically
 function populateDOM() {
+  // Set a counter variable for loop
   var counter = 4;
-  var div = "";
-  var button;
+  var div, button;
   for (var i = 0; i < counter; i++) {
+    // Create an HTML button element
     button = document.createElement('button');
+    // Set the button's text
     button.innerText = `Button ${i+1}`;
+    // Create an HTML div element
     div = document.createElement('div');
+    // Set the div's text
     div.innerText = `DIV ${i+1}:  `;
-    div.setAttribute('class', 'div');
+    // Give the div a 'divs' class
+    div.setAttribute('class', 'divs');
+    // Append the button to the div
     div.appendChild(button);
+    // Append the div to the outputDiv
     outputDiv.appendChild(div);
   };
 };
 
+// Call the populateDOM function
 populateDOM();
