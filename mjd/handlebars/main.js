b0fcb75099d9f69e87d8d9a1be2c1716b661a188
Dominic Serrano <dmsrojo@gmail.com>
2017-01-05 11:05:16 -0600

    Added helper functions and examples accessing nested objects

--- mjd/handlebars/main.js
+++ mjd/handlebars/main.js
@@ -1,13 +1,32 @@
 var data = {
   title: 'Hello World',
-  p1: 'I am the first p tag',
-  p2: 'I am the second p tag'
-}
+  group: {
+    list: ['Array Index 0', 'Array Index 1']
+  },
+  people: [
+    { firstName: 'Laura', lastName:'C' },
+    { firstName: 'Donna', lastName:'B' },
+    { firstName: 'George', lastName:'Farlan' }
+  ],
+  things: [
+    { name: 'Thing 1' },
+    { name: 'Thing 2' },
+    { name: 'Thing 3' }
+  ]
+};
 
+Handlebars.registerHelper('list', function(items, options) {
+  var out = "<p>";
+
+  for(var i=0, l=items.length; i<l; i++) {
+    out = out + options.fn(items[i]) + "</p>";
+  };
+
+  return out;
+});
+
+// Create template function and output to DOM
 var entryHTML = $('#partial').html();
-
 var entryTemplate = Handlebars.compile(entryHTML);
-
 var entryOutput = entryTemplate(data);
-
 $('#hb-output').append(entryOutput);
