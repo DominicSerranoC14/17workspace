112df09195fa6908bacf5b9221f5d830be6ce8cc
Dominic Serrano <dmsrojo@gmail.com>
2017-01-09 20:12:06 -0600

    $.ajax post and get method practice

--- mjd/movieHistory/main.js
+++ mjd/movieHistory/main.js
@@ -1,27 +1,44 @@
 'use strict';
 
 var url = 'http://www.omdbapi.com/';
+var FBurl = 'https://js-space-adventure.firebaseio.com/';
 var movieSearchHTML;
-
 
 function getSearch(searchWord) {
   $.ajax({
-    url: `${url}/${searchWord}`,
-    dataType: 'json'
-  }).then(function(data) {
+    url: `${url}/${searchWord}`
+  })
+  .then(function(data) {
     var search = data.Search;
 
-    search.forEach(each => {
+    search.forEach((each, i) => {
       $('#output').append(`
-        <div class="card">
+        <div id="${i}" class="card">
         <h4>${each.Title}</h4>
         <p>${each.Year}</p>
         <img src="${each.Poster}"></img>
         </div>
       `);
     });
+
+    $('.card').click(function(e) {
+      var currentCardId = $(e.currentTarget).attr('id');
+      var currentCardObj = search[currentCardId];
+
+      postMovie(currentCardObj);
+    });
   });
-}
+};
 
+function postMovie(movieObj) {
+  $.ajax({
+    method: 'POST',
+    url: `${FBurl}movie.json`,
+    data: JSON.stringify(movieObj)
+  })
+  .then(function(status) {
+    console.log(status);
+  });
+};
 
-getSearch('?s=Batman');
+getSearch('?s=Godzilla');
