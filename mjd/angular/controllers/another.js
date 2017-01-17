5c766dd6b1d37f17ed335d15b021666b64fdd6d2
Dominic Serrano <dmsrojo@gmail.com>
2017-01-17 10:44:43 -0600

    Added  example, ng-repeat and ng-if

--- mjd/angular/controllers/another.js
+++ mjd/angular/controllers/another.js
@@ -1,7 +1,15 @@
-'use-strict';
+'use strict';
 
-app.controller('AnotherCtrl', function($scope) {
+app.controller('AnotherCtrl', function($scope, $http) {
 
   $scope.message = 'I am from AnotherCtrl!';
+
+  $scope.list = ['Marlin', 'George', 'Lisa'];
+
+  // Populate $scope.list with a local $http call
+  $http.get('list.json')
+  .then(listData => {
+    $scope.list = listData.data.list.map(each => `${each.name} from $http!`);
+  });
 
 });
