cad36340a62e27d9d709658db240dbb1cba91aab
Dominic Serrano <dmsrojo@gmail.com>
2017-01-19 10:59:14 -0600

    Added rootCtrl logic, navbar and a form for weather search input

--- modernFW/angularWeather/app/app.js
+++ modernFW/angularWeather/app/app.js
@@ -9,9 +9,21 @@ angular
         controller: 'RootCtrl',
         templateUrl: 'partials/root.html'
       })
+      .when('/weather/:zipcode', {
+        controller: 'WeatherCtrl',
+        templateUrl: 'partials/weather.html'
+      })
       .otherwise('/');
 
   })
-  .controller('RootCtrl', function() {
+  .controller('RootCtrl', function($scope) {
 
-  });
+    $scope.searchForecast = (search) => {
+      console.log("search", search);
+    };
+
+  })
+  .controller('WeatherCtrl', function($scope) {
+
+
+  })
