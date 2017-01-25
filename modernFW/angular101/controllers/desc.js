7400c2aedb3ae96e7d856fd5029ebfdd79559da4
Dominic Serrano <dmsrojo@gmail.com>
2017-01-25 09:53:15 -0600

    Fixed loading error with list.json

--- modernFW/angular101/controllers/desc.js
+++ modernFW/angular101/controllers/desc.js
@@ -2,7 +2,7 @@ app.controller('DescCtrl', function($scope, $http, $routeParams) {
 
   $scope.personId = $routeParams.id;
 
-  $http.get('list.json')
+  $http.get('json/list.json')
   .then(( {data: { list }} ) => {
     $scope.personObj = list[$scope.personId];
   });
